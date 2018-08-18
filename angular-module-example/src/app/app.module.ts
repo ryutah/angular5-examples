1e11ae7140979517a554a4d4e3287d709d64733a
ryutah <ryuta0221@gmail.com>
2018-08-18 21:42:37 +0900

    Add: module example

---
+++ angular-module-example/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerDashboardModule } from './customer-dashboard/customer-dashboard.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    CustomerDashboardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular-module-example/src/app/customer-dashboard/customer-dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CustomerDashboardComponent],
+  exports: [CustomerDashboardComponent],
+})
+export class CustomerDashboardModule {}
